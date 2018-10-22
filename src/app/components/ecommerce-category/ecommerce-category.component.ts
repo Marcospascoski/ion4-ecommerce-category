5d32d6f02e898e8e748350adfd4dd3e8c98b8cde
kanomdook <kanung.2535@gmail.com>
2018-10-22 17:40:09 +0700

    [edit] #2

--- src/app/components/ecommerce-category/ecommerce-category.component.ts
+++ src/app/components/ecommerce-category/ecommerce-category.component.ts
@@ -8,12 +8,20 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class EcommerceCategoryComponent implements OnInit {
   @Input() title = 'title';
   @Input() items: Array<any> = [];
+  items1: Array<any> = [];
+  items2: Array<any> = [];
   @Output() itemSelected = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
-
+    this.items.forEach((el, i) => {
+      if (i < 5) {
+        this.items1.push(el);
+      } else {
+        this.items2.push(el);
+      }
+    });
   }
 
   selectItem(item) {
